ddc0ca18468bb69786a8fd66960323f898646f6b
JetsonAI <96120477+jetsonai@users.noreply.github.com>
2023-06-28 21:01:52 +0900

    Update pi_camera.py

--- Basic_Install/pi_camera.py
+++ Basic_Install/pi_camera.py
@@ -6,7 +6,7 @@ gst_str = ("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)224, height=
 import cv2
 import numpy as np
 
-def Video(openpath):
+def videoProcessing(openpath):
     cap = cv2.VideoCapture(openpath)
     if cap.isOpened():
         print("Video Opened")
@@ -15,38 +15,28 @@ def Video(openpath):
         print("Program Abort")
         exit()
     fps = cap.get(cv2.CAP_PROP_FPS)
-    print('fps : {}'.format(fps))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
-    #fourcc = cv2.VideoWriter_fourcc('m','p','4','v') with *.mp4 save
 
-    cv2.namedWindow("Input", cv2.WINDOW_GUI_EXPANDED)
+    cv2.namedWindow("Output", cv2.WINDOW_GUI_EXPANDED)
 
-    try:
-        while cap.isOpened():
-            # Capture frame-by-frame
-            ret, frame = cap.read()
-            if ret:
+    while cap.isOpened():
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret:
 
-                # Display the resulting frame
-                cv2.imshow("Input", frame)
-
-            else:
-                break
-            # waitKey(int(1000.0/fps)) for matching fps of video
-            if cv2.waitKey(int(1000.0/fps)) & 0xFF == ord('q'):
-                break
-    except KeyboardInterrupt:  
-        print("key int")
-        cap.release()
-        cv2.destroyAllWindows()
-        exit()
+            # Display the resulting frame
+            #cv2.imshow("Input", frame)
+            cv2.imshow("Output", frame)
+        else:
+            break
+        # waitKey(int(1000.0/fps)) for matching fps of video
+        if cv2.waitKey(int(1000.0/fps)) & 0xFF == ord('q'):
+            break
     # When everything done, release the capture
     cap.release()
-
     cv2.destroyAllWindows()
     return
    
 if __name__=="__main__":
-    Video(gst_str)
+    videoProcessing(gst_str)
